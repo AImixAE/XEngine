aece6b7ced6c772943db49a6268014032bab19bd
AImixAE Mocha <AImixAE@outlook.com>
2025-08-02 09:15:46 +0800

    feat(core): 初始化日志模块并添加日志宏定义
    - 在入口点初始化日志模块
    - 添加 XE_CORE_* 和 XE_* 宏用于不同级别的日志记录
    - 更新 Log.h 文件，包含新的日志宏定义

--- Core/Include/XEngine/Core/EntryPoint.h
+++ Core/Include/XEngine/Core/EntryPoint.h
@@ -3,10 +3,14 @@
 #include "XEngine/Core/Application.h"
 #include "XEngine/Core/Log.h"
 
+using namespace XEngine::Core::Logger;
+
 extern XEngine::Core::Application* XEngine::Core::CreateApplication();
 
 int main(int argc, char** argv) {
     XEngine::Core::Log::Init();
+    XE_CORE_WARN("Initialized Logger Module!");
+    XE_INFO("Initialized Logger Module!");
 
     auto app = XEngine::Core::CreateApplication();
 
--- Core/Include/XEngine/Core/Log.h
+++ Core/Include/XEngine/Core/Log.h
@@ -1,12 +1,28 @@
 #pragma once
 
 #include <memory>
-
 #include "spdlog/spdlog.h"
 
 namespace XEngine {
 namespace Core {
 extern "C" {
+namespace Logger {
+#define XE_CORE_ERROR(...) \
+    XEngine::Core::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define XE_CORE_WARN(...) XEngine::Core::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define XE_CORE_INFO(...) XEngine::Core::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define XE_CORE_TRACE(...) \
+    XEngine::Core::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define XE_CORE_FATAL(...) \
+    XEngine::Core::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define XE_ERROR(...) XEngine::Core::Log::GetClientLogger()->error(__VA_ARGS__)
+#define XE_WARN(...) XEngine::Core::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define XE_INFO(...) XEngine::Core::Log::GetClientLogger()->info(__VA_ARGS__)
+#define XE_TRACE(...) XEngine::Core::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define XE_FATAL(...) XEngine::Core::Log::GetClientLogger()->fatal(__VA_ARGS__)
+}  // namespace Logger
+
 class Log {
    public:
     Log();
